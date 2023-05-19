0aaf851aeec4ebfe941e72bbe095a82fd0ed72ab
이원종 <dnjswhd1348@naver.com>
2023-05-19 14:50:30 +0900

    gunicorn test

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,3 +2,4 @@ from .base import *
 
 ALLOWED_HOSTS = []
 DEBUG = False
+ROOT_URLCONF = 'config.urls'
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,4 @@ ALLOWED_HOSTS = ['3.34.74.107']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+ROOT_URLCONF = 'config.urls'
